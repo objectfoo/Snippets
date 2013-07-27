1fd3a9958837bc380ccd103d38bd4ca3a2655c8b
Satake <satake@objectfoo.com>
2013-07-26 19:41:36 -0700

    add already scrolled check

--- autoHideMobileAddressbar.js
+++ autoHideMobileAddressbar.js
@@ -1,15 +1,19 @@
 /**
 * iOS needs setTimeout but works w/ 0,0
 * android needs 0,1
+* ignore if page is scrolled
 **************************************/
 (function (window, addEvent) {
-  window[addEvent] && window[addEvent]('load', function () {
-    setTimeout(function () {
-      window.scrollTo(0, 1);
-    }, 0);
-  });
-}(window, 'addEventListener'));
+  if (!window.location.hash && window[addEvent]) {
+    window[addEvent]('load', function () {
+      if ((window.pageYOffset || document.body.scrollTop || 0) < 20) {
+        window.scrollTo(0, 1);
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+})(window, 'addEventListener');
 
 
 // Minified
-(function(a,b){a[b]&&a[b]("load",function(){setTimeout(function(){a.scrollTo(0,1)},0)})})(window,"addEventListener");
+(function(a,b){if(!a.location.hash&&a[b])a[b]("load",function(){20>(a.pageYOffset||document.body.scrollTop||0)&&(a.scrollTo(0,1),a.scrollTo(0,0))})})(window,"addEventListener");
