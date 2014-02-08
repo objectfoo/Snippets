8c760d0dbfb232422cd327c9fbe1b0f54932f04a
Satake <satake@objectfoo.com>
2014-02-08 08:49:01 -0800

    Update supportsFocusIn.js

--- supportsFocusIn.js
+++ supportsFocusIn.js
@@ -17,7 +17,8 @@ var supportsFocusin = (function() {
 
 	a.addEventListener ? a.addEventListener('focusin', addSupport) : a.onfocusin = addSupport;
 
-	parent.appendChild(a).focus();
+	// beginning of page so it doesn't scroll down. may impede other scroll actions though
+	parent.insertBefore(a, parent.firstChild).focus().blur();
 
 	parent.removeChild(a);
 
